d6fff3780b66b319851685be1a50006b6c38de9b
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2017-11-28 01:42:18 +0900

    step3

--- IdeaProjects/Report4/src/report4.java
+++ IdeaProjects/Report4/src/report4.java
@@ -2,8 +2,10 @@ public class report4 {
     public static void main(String[] args) {
         try {
             String str;
-            str = null;
+            str = "３．１４";
             System.out.println(str.length());
+            double value;
+            value = Double.parseDouble(str);
         } catch (NullPointerException e) {
             System.out.println("NullPointerExceptionが発生しました");
             System.out.println(e.getMessage()) ;
