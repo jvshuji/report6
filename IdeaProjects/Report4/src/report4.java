7f4d5f90bed578d0aab9fc4ae1ad3f630aaf2340
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2017-11-28 01:08:46 +0900

    step1

--- IdeaProjects/Report4/src/report4.java
+++ IdeaProjects/Report4/src/report4.java
@@ -1,7 +1,12 @@
 public class report4 {
     public static void main(String[] args) {
-    String str ;
-    str = null ;
-    System.out.println(str.length() );
+        try {
+            String str;
+            str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
         }
+    }
 }
