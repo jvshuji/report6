e842c3ebf70dd89ecfbf462bda34ecc67b9b437b
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2017-12-16 14:45:45 +0900

    Report5 first commit

---
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/Enemy.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e175712;
+
+public class Enemy extends  LivingThing{
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name,maximumHP,attack);
+    }
+    public void wounded(int damage) {
+        setHitPoint(getHitPoint() - damage);
+        if (getHitPoint() < 0) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+
+}
---
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/Hero.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e175712;
+
+public class Hero extends LivingThing {
+
+    public Hero (String name, int maximumHP, int attack) {
+        super(name,maximumHP,attack);
+    }
+
+    public void wounded(int damage){
+        setHitPoint(getHitPoint() - damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+        }
+    }
+}
