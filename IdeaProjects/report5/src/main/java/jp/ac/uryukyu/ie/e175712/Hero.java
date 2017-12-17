c13f0a575190ee5dbd0a4c3f0c32426019cccc70
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2017-12-17 21:28:19 +0900

    report5-1 third commit

--- IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/Hero.java
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/Hero.java
@@ -6,6 +6,29 @@ public class Hero extends LivingThing {
         super(name,maximumHP,attack);
     }
 
+    @Override
+    public void attack(LivingThing opponent){
+        int damage = (int) (Math.random() * getAttack());
+        int random = (int) (Math.random() * 100);
+        if (getDead() == false) {
+            if (random < 40) {
+                if (damage == 0) {
+                    System.out.printf("%sの攻撃！会心の一撃！！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                } else {
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage * 2);
+                    opponent.wounded(damage * 2);
+                }
+            } else {
+                if (damage == 0) {
+                    System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                } else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }
+            }
+        }
+    }
+
     public void wounded(int damage){
         setHitPoint(getHitPoint() - damage);
         if( getHitPoint() < 0 ) {
--- IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/LivingThing.java
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175712/LivingThing.java
@@ -21,11 +21,6 @@ public class LivingThing {
         return name;
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param opponent 攻撃対象
-     */
     public void attack(LivingThing opponent) {
         if (isDead() == false) {
             int damage = (int) (Math.random() * attack);
@@ -34,11 +29,6 @@ public class LivingThing {
         }
     }
 
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
