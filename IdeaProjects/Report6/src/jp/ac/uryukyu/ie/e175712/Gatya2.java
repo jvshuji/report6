dd551325677465b063d4b678da874b7e067f26ef
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2018-01-16 08:39:58 +0900

    fourth commit

---
+++ IdeaProjects/Report6/src/jp/ac/uryukyu/ie/e175712/Gatya2.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e175712;
+
+public class Gatya2 extends Gatya {
+    public void Type2() {
+        Gatya gatya = new Gatya();
+        gatya.play("☆2キャラ\nエスターク",
+                "☆2キャラ\nゴーレム",
+                "☆3キャラ\nキラーマシン",
+                "☆5キャラ\nゾーマ",
+                "☆5キャラ\n竜王");
+    }
+}
