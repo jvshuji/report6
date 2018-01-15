b531b5c3b72d6461687737c05096554455e00c8d
Shuji WATANABE <e175712@ie.u-ryukyu.ac.jp>
2018-01-16 08:28:58 +0900

    first commit

---
+++ IdeaProjects/Report6/src/jp/ac/uryukyu/ie/e175712/Main.java
@@ -0,0 +1,30 @@
+package jp.ac.uryukyu.ie.e175712;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("ガチャを選択してください\n・ガチャ(1)\n・レアガチャ(2)\n");
+        Scanner in = new Scanner(System.in);
+        String input;
+        input = in.nextLine();
+        while (input.equals("ガチャ")||input.equals("がちゃ")||input.equals("1") ==false
+                ||
+                input.equals("レアガチャ")|| input.equals("れあがちゃ")||input.equals("2") == false) {
+            if (input.equals("ガチャ")||input.equals("がちゃ")||input.equals("1")==true) {
+                Gatya1 gatya1 = new Gatya1();
+                gatya1.Type1();
+                break ;
+            } else {
+                if (input.equals("レアガチャ")|| input.equals("れあがちゃ")||input.equals("2") == true) {
+                    Gatya2 gatya2 = new Gatya2();
+                    gatya2.Type2();
+                    break ;
+                } else {
+                    System.out.println("ガチャを選択してください");
+                    input = in.nextLine();
+                }
+            }
+        }
+    }
+}
--- IdeaProjects/report5/src/test/java/jp/ac/uryukyu/ie/e175712/EnemyTest.java
+++ IdeaProjects/report5/src/test/java/jp/ac/uryukyu/ie/e175712/EnemyTest.java
@@ -1,20 +1,20 @@
 package jp.ac.uryukyu.ie.e175712;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class EnemyTest {
-    @Test
-    void attack() {
-        int heroHP = 10;
-        Hero hero = new Hero("テスト勇者", heroHP, 5);
-        Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.setDead(true);
-        for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        @Test
+        void attack() {
+            int heroHP = 10;
+            Hero hero = new Hero("テスト勇者", heroHP, 5);
+            Enemy enemy = new Enemy("テストスライム", 6, 3);
+            enemy.setDead(true);
+            for(int i=0; i<10; i++) {
+                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            }
+            assertEquals(heroHP, hero.getHitPoint());
         }
-        assertEquals(heroHP, hero.getHitPoint());
-    }
 
 }
